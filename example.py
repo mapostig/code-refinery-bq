3ec7ddcbbef38756c61c73f1cdfc272828b6787f
María Postigo <62014302+mapostig@users.noreply.github.com>
2021-06-08 14:02:23 +0200

    Update example.py

--- example.py
+++ example.py
@@ -12,12 +12,12 @@ def factorial(n):
     """
     Computes the factorial of n.
     """
-if n < 0:
-   raise ValueError('received negative input')
-result = 1
-for i in range(1, n + 1):
-   result *= i
-return result
+    if n < 0:
+       raise ValueError('received negative input')
+    result = 1
+    for i in range(1, n + 1):
+       result *= i
+    return result
 
 
 def test_negative_factorial():
