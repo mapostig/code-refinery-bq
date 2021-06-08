a3fd110b7b2f15eeb4fa8462b889fd5e811c4dac
María Postigo <62014302+mapostig@users.noreply.github.com>
2021-06-08 13:41:24 +0200

    Update temperature_test.py

--- temperature_test.py
+++ temperature_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-def test_fahrenheit_to_celsius(temp_f):
+def test_fahrenheit_to_celsius():
     temp_c = fahrenheit_to_celsius(temp_f = 100)
     expected_result = 37.7777777
     assert abs(temp_c - expected_result) < 1.0e-6
