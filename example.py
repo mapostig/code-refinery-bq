5591a18c21d6aa43e6cb500609dda7f80da0eb8d
María Postigo <62014302+mapostig@users.noreply.github.com>
2021-06-08 13:40:32 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,18 +1,18 @@
 import pytest
 
-def add(a, b):
-    return a + b
-
-
-def test_add():
-    assert add(1, 2) == 3
-#     assert add('space', 'ship') == 'spaceship'
-
-
-
 # def add(a, b):
 #     return a + b
 
 
 # def test_add():
-#     assert pytest.approx(add(0.1, 0.2), 0.3)
+#     assert add(1, 2) == 3
+# #     assert add('space', 'ship') == 'spaceship'
+
+
+
+def add(a, b):
+    return a + b
+
+
+def test_add():
+    assert pytest.approx(add(0.1, 0.2), 0.3)
