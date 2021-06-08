2be77a0063deac10f365faa6f789c457de32a7f5
María Postigo <62014302+mapostig@users.noreply.github.com>
2021-06-08 12:10:38 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
+# def add(a, b):
+#     return a + b
+
+
+# def test_add():
+#     assert add(2, 3) == 5
+#     assert add('space', 'ship') == 'spaceship'
+
 def add(a, b):
     return a + b
 
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
+    assert add(0.1, 0.2) == 0.3
