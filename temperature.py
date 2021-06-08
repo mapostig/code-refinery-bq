ca4dcd8efdf14bc6182ea555a5640b816fdf1853
María Postigo <62014302+mapostig@users.noreply.github.com>
2021-06-08 12:02:49 +0200

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,4 +1,5 @@
-temperature = 10
-
-def print_temperature():
-    print(temperature)
+def fahrenheit_to_celsius(temp_f):
+    """Convert temperature in Fahrenheit to Celsius
+    """
+    temp_c = (temp_f-32)*(5.0/9.0)
+    return temp_c
